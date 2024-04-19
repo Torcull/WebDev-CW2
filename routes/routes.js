e6a6d9c7baecdd07a8ac8aab887cb732ea2ec253
Torcull <torcullalasdairmacleod@gmail.com>
2024-04-19 12:06:48 +0100

    fixed!

--- routes/routes.js
+++ routes/routes.js
@@ -6,18 +6,16 @@ const { verify } = require("../auth/auth");
 
 router.get("/login", controller.login_page);
 router.post("/login", login, controller.handle_login);
-router.get("/logout", verify, controller.logout);
+router.get("/logout", controller.logout);
 router.get("/donate", verify, controller.donate);
 router.get("/", controller.landing_page);
 router.get("/foods", verify, controller.foods);
 router.get("/foods/:donatorname", controller.show_donator_foods);
-router.get("/register", controller.register);
 router.get("/apples", controller.apples);
 
 router.post("/donate", controller.post_donation);
 router.get("/register", controller.register_page);
 router.post("/register", controller.post_new_user);
-router.post("/routeName", login, function (req, res) {}); //remove this when done
 
 router.use(function (req, res) {
   res.status(404);
